48431cd231da4a77d2158e0c536c222bbd81a8d0
Pedro Manoel <dev.pedromanoel@pm.me>
2025-04-10 00:44:33 -0300

    Initial Commit

---
+++ Koat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Koat
+//
+//  Created by Pedro Manoel on 10/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KoatTests/KoatTests.swift
@@ -0,0 +1,17 @@
+//
+//  KoatTests.swift
+//  KoatTests
+//
+//  Created by Pedro Manoel on 10/04/25.
+//
+
+import Testing
+@testable import Koat
+
+struct KoatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
